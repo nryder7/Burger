77da3bd268cd445ef943c43d1e3c67cde7159a6e
Nicholas Ryder <nryder7@gmail.com>
2020-09-10 21:56:59 -0400

    devour burger fixed

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,10 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newDevoured = $(this).data("newdevoured");
-
+  
     var newDevouredState = {
-      devoured: newDevoured
+      devoured: 1
     };
 
     // Send the PUT request.
@@ -14,7 +13,7 @@ $(function() {
       data: newDevouredState
     }).then(
       function() {
-        console.log("changed devoured to", newDevoured);
+        console.log("changed devoured to", true);
         
         location.reload();
       }
